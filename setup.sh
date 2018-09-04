b7f42af71d47601ce7efc1972ef0a52f7e4c9453
Kim DoHyeon <k.dohvis@gmail.com>
2018-09-04 15:49:56 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+
 python() {
   sudo apt-get update
   sudo apt-get install -y python-dev python-pip python3-dev python3-pip
@@ -13,22 +17,21 @@ install_omz() {
   sudo apt-get install -y curl
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-  chsh -s /bin/zsh
 }
 
+
 install_zsh_theme() {
-  mkdir $HOME/.zFunctions  
+  #mkdir $HOME/.zFunctions
   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
   ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-  echo "fpath=( "$HOME/.zfunctions" $fpath )" >> ~/.zshrc
-  echo "autoload -U promptinit; promptinit; prompt spaceship" >> ~/.zshrc
   sed -i "s/ZSH_THEME=\"[a-z]*\"/ZSH_THEME=\"spaceship\"/g" ~/.zshrc
 }
 
 install_zsh_plugins() {
-  mkdir $HOME/.oh-my-zsh/custom/plugins
-  git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  sed -i "s/^plugins=(/plugins=\(\n  zsh-autosuggestions\n/g" ~/.zshrc
 }
+
 neovim() {
   sudo apt-get install -y software-properties-common
   sudo add-apt-repository ppa:neovim-ppa/stable
